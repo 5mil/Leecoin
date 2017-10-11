9ad8defb9fe326b2d5cc9f4f94f368b05cf22971
5mil <jdaurio09@gmail.com>
2017-10-11 14:01:11 -0400

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -982,7 +982,7 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
 }
 
 // miner's coin base reward
-int64_t GetProofOfWorkReward(int64_t nFees)
+//int64_t GetProofOfWorkReward(int64_t nFees)
 {
 
     int64_t nSubsidy = 1 * COIN;
